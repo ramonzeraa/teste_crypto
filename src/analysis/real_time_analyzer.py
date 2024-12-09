fe8ab27d63791f6ca0055d33659658f2b2af92ac
Ramon Cândido <ramoncandido64@gmail.com>
2024-12-09 17:02:41 -0300

    Criado analisador em tempo real

---
+++ src/analysis/real_time_analyzer.py
@@ -0,0 +1,14 @@
+class RealTimeAnalyzer:
+    def __init__(self):
+        self.market_data = None
+        self.news_feed = None
+        self.technical_indicators = {}
+        self.confidence_score = 0.0
+
+    def analyze_current_market(self):
+        # Análise simultânea de:
+        # - Preço em tempo real
+        # - Indicadores técnicos
+        # - Sentimento do mercado
+        # - Notícias relevantes
+        pass 
